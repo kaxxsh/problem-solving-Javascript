80a5958720f444a2a63b194a1e5228ecd932fbf8
kaxxsh <kameshwaran2002@gmail.com>
2023-09-04 11:41:31 +0530

    update

--- Find duplicates in an array.js
+++ Find duplicates in an array.js
@@ -1,16 +1,11 @@
-let arr = [1, 4, 5, 6, 8, 74, 12, 1, 41, 23, 5, 4, 5, 68, 74, 5, 6, 7, 4, 1];
-
-let dublicate = [];
-
+let arr = [2, 2, 4, 4, 1];
+const duplicates = [];
 for (let i = 0; i < arr.length; i++) {
-  flag = 0;
   for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] === arr[j]) {
-      flag++;
+    if (arr[i] === arr[j] && !duplicates.includes(arr[i])) {
+      duplicates.push(arr[i]);
     }
   }
-
-  if (flag === 0) dublicate.push(arr[i]);
 }
 
-dublicate.map((i) => console.log(i));
+console.log(duplicates);
